97b319a32f12d9fbb926548bf52c1184e4965fa6
Krutagna Patel <kpatel.kp1666@gmail.com>
2022-09-21 11:18:03 +0530

    .

---
+++ Javascript/script.js
@@ -0,0 +1,57 @@
+
+let year = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+year.textContent = currentYear; 
+
+let header = document.querySelector("header");
+let buttonNavigation = document.querySelector(".btn--mobile-nav");
+
+buttonNavigation.addEventListener("click", () => {
+    header.classList.toggle("nav-open");
+})
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile naviagtion
+    if (link.classList.contains("main-nav-link"))
+      header.classList.toggle("nav-open");
+  });
+});
+
+
+heroSection = document.querySelector(".section-hero");
+// Sticky navigation
+const obs = new IntersectionObserver(function(entries){
+  const ent = entries[0];
+  if (ent.isIntersecting === false) {
+    document.querySelector("body").classList.add("sticky");
+  } else {
+    document.querySelector("body").classList.remove("sticky");
+  }
+},
+ {
+  // In the viewport
+  root: null, 
+  threshold: 0,
+  rootMargin: "-80px"
+});
+obs.observe(heroSection);
+
